c7b0d7598ea422d357012a44a2550ee621a54506
DorraBenAissa <dorra.benaissaghariani@gmail.com>
2024-01-26 10:35:35 +0100

    Les fonctions

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ const slides = [
         "image": "slide4.png",
         "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
     }
-]// Déclaration d'une structure de données (JSON) comme étant une constantes ==> des textes + les noms des images 
+]// Déclaration d'une structure de données (JSON)(le tableau “slides”) comme étant une constante ==> des textes + les noms des images 
 
 
 const bannerImg = document.querySelector('.banner-img'); // Pour les bannieres 
@@ -38,6 +38,7 @@ updateDots(currentIndex); // Mettez à jour les bullet points pour le premier sl
 
 //// Étape 2 : Ajouter des Event Listeners sur les flèches 
 
+
 // Mettre un event listener sur la flèche gauche pour le clic 
 arrowLeft.addEventListener('click', function () {
     currentIndex = (currentIndex - 1);
@@ -47,7 +48,40 @@ arrowLeft.addEventListener('click', function () {
 
 // Mettre un event listener sur la flèche droite pour le clic 
 arrowRight.addEventListener('click', function () {
-    currentIndex = (currentIndex + 1);
+    currentIndex = (currentIndex + 1); // 
     updateCarousel(currentIndex, 'right');
     updateDots(currentIndex); // Mettre à jour Les bullet points
 }); 
+
+// Fonction pour mettre à jour les points indicateurs (boucle fort : foreach)
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
+        } else {
+            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
+        }
+    });
+}
+
+// Fonction pour mettre en place le défilement infini sur le carrousel  
+function updateCarousel(index, direction) {
+    //correction du bug pour la première et la dernière image
+    if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // Dans tous les cas, le texte change en accord avec l’image montrée.
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    // Ajouter un console.log ou une alert() pour tester le fonctionnement des event listeners.
+    console.log(`Clic sur la flèche ${direction}`);
+}
