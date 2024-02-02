8960c866d53e338e634111088450f9e2dc83a575
DorraBenAissa <dorra.benaissaghariani@gmail.com>
2024-02-02 15:11:22 +0100

    Correction Mentor : Synchronisation entre le nombre des points et le nombres des images

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 
-//// Étape 1 : Ajouter les constantes et les variables: 
+
+//// Étape 1 : Ajouter les constantes et les variables:
 
 const slides = [
     {
@@ -18,70 +19,68 @@ const slides = [
         "image": "slide4.png",
         "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
     }
-]// Déclaration d'une structure de données (JSON)(le tableau “slides”) comme étant une constante ==> des textes + les noms des images 
+]; // Déclaration d'une structure de données (JSON)(le tableau “slides”) comme étant une constante ==> des textes + les noms des images
 
-
-const bannerImg = document.querySelector('.banner-img'); // Pour les bannieres 
-const arrowLeft = document.querySelector('.arrow_left'); // Pour les fléches
-const arrowRight = document.querySelector('.arrow_right'); // Pour les fléches
-const dots = document.querySelectorAll('.dot'); // Sélection tous les points (Les bullet points)
-
-
-
+const bannerImg = document.querySelector('.banner-img'); // Pour les bannières
+const arrowLeft = document.querySelector('.arrow_left'); // Pour les flèches
+const arrowRight = document.querySelector('.arrow_right'); // Pour les flèches
+const dotsContainer = document.querySelector('.dots'); // Pour les flèches
 
 let currentIndex = 0;
 
 // Afficher le premier slide au chargement de la page
-updateCarousel(currentIndex, 'démarrage');
-updateDots(currentIndex); // Mettez à jour les bullet points pour le premier slide
+updateCarousel(currentIndex);
 
 
-//// Étape 2 : Ajouter des Event Listeners sur les flèches 
+//// Étape 2 : Ajouter des Event Listeners sur les flèches
 
-
-// Mettre un event listener sur la flèche gauche pour le clic 
+// Mettre un event listener sur la flèche gauche pour le clic
 arrowLeft.addEventListener('click', function () {
-    currentIndex = (currentIndex - 1);
-    updateCarousel(currentIndex, 'left');
-    updateDots(currentIndex); // Mettre à jour les bullet points
+    currentIndex = currentIndex - 1 < 0 ? slides.length - 1 : currentIndex - 1;
+    updateCarousel();
+    updateDots();
 });
 
-// Mettre un event listener sur la flèche droite pour le clic 
+// Mettre un event listener sur la flèche droite pour le clic
 arrowRight.addEventListener('click', function () {
-    currentIndex = (currentIndex + 1); // 
-    updateCarousel(currentIndex, 'right');
-    updateDots(currentIndex); // Mettre à jour Les bullet points
-}); 
+    currentIndex = currentIndex + 1 >= slides.length ? 0 : currentIndex + 1;
+    updateCarousel();
+    updateDots();
+});
 
-// Fonction pour mettre à jour les points indicateurs (boucle fort : foreach)
-function updateDots(index) {
-    dots.forEach((dot, i) => {
-        if (i === index) {
-            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
-        } else {
-            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
-        }
-    });
+// Fonction pour mettre à jour les points indicateurs
+function updateDots() {
+    const dots = document.querySelectorAll('.dot'); // Sélection de tous les points (Les bullet points)
+
+    if (document.querySelector('.dot_selected') !== null) {
+        document.querySelector('.dot_selected').classList.remove('dot_selected');
+    }
+    dots[currentIndex].classList.add('dot_selected');
 }
 
-// Fonction pour mettre en place le défilement infini sur le carrousel  
-function updateCarousel(index, direction) {
-    //correction du bug pour la première et la dernière image
-    if (currentIndex === -1 && direction === 'left') {
-        currentIndex = slides.length - 1;
-    } else if (currentIndex === slides.length && direction === 'right') {
-        currentIndex = 0;
-    }
-
+// Fonction pour mettre en place le défilement infini sur le carrousel
+function updateCarousel() {
     // Mettre à jour l'image
-    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
-    bannerImg.src = imagePath;
-    bannerImg.alt = `Slide ${currentIndex + 1}`;
+    bannerImg.src = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.alt = slides[currentIndex].tagLine;
 
     // Dans tous les cas, le texte change en accord avec l’image montrée.
-    const tagLine = slides[currentIndex].tagLine;
-    document.querySelector('p').innerHTML = tagLine;
-
-    // Ajouter un console.log ou une alert() pour tester le fonctionnement des event listeners.
-    console.log(`Clic sur la flèche ${direction}`);
+    document.querySelector('p').innerHTML = slides[currentIndex].tagLine;
 }
+
+
+
+// Fonction pour générer les points indicateurs dynamiquement
+function createDots() {
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dotsContainer.appendChild(dot);
+    }
+}
+
+// Appeler la fonction pour créer les points indicateurs
+createDots();
+
+
+updateDots(currentIndex);
